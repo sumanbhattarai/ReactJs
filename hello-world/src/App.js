4f0a2db70eaf3c59a3151160ad4b99809c4bb23a
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-12-31 18:13:38 +0545

    commented out another way

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component{
       <div className="App">
         <Greet name={this.state.name[0]} />
         <Greet click={this.swapName.bind(this , 'SB')} name={this.state.name[1]} >I am 20 years old. </Greet>
+        {/* we can also do as click ={ ()=> this.swapName('SB')} but its not a good idea. */}
         <button onClick={this.swapName.bind(this , 'Max')}>Swap Name</button>
     </div>
     );
