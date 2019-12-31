09fc685f2db670c9dfae40fc76c6b70821cf91fa
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-12-31 19:59:40 +0545

    Rendering element conditionally

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,7 +5,8 @@ import Greet from './components/Greet' ;
 
 class App extends React.Component{
   state = {
-    name : [ 'Suman' , 'Sujan']
+    name : [ 'Suman' , 'Sujan'],
+    show : true 
   }
 
   swapName = (newName) => {
@@ -22,13 +23,34 @@ class App extends React.Component{
     })
 
   }
+
+  changeShowState = () =>{
+    const showStatus = this.state.show ;
+    this.setState({
+      show : !showStatus
+    })
+  }
+
+  buttonName = ()=> {
+    if(this.state.show === true) 
+      return 'Hide' ;
+    else
+      return 'Show' ;
+  }
+
   render(){
     return (
       <div className="App">
+        {
+          this.state.show === true ?
+        <div>
         <Greet change={this.changeName} name={this.state.name[0]} />
         <Greet click={this.swapName.bind(this , 'SB')} name={this.state.name[1]} >I am 20 years old. </Greet>
         {/* we can also do as click ={ ()=> this.swapName('SB')} but its not a good idea. */}
+        </div> : null 
+        }
         <button onClick={this.swapName.bind(this , 'Max')}>Swap Name</button>
+        <button onClick={this.changeShowState}>{this.buttonName()}</button>
     </div>
     );
   }
