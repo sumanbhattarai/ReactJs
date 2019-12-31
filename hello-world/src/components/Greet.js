3b5f450c169dd46e52a171c16c6e993624197e30
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-12-31 18:43:14 +0545

    Adding some css to the component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,9 +1,10 @@
 import React from 'react' ;
+import './Greet.css'
 
 
 const greet = (props) => {
     return(
-        <div>
+        <div className = "Greet">
             <p onClick={props.click}>Hi , my name {props.name}.</p>
             <p>{props.children}</p>
             <input value={props.name} onChange={props.change} />
